1454c557e0ed1d8f9c06e275fde72c2164442713
Deryn Brown <Deryn.2017@gmail.com>
2022-10-20 01:35:34 -0500

    commit tree 14c3610b1aede7a2818e6e30d528eb6fe92c96ea

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        !!freebies.where(item_name: item_name).first || false
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev_id: dev.id) unless freebie.dev_id != self.id
+    end
 end
